ca16f6f0f4ffe8ebacc91a84330cc162a18aad63
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-04-23 18:19:41 -0500

    ✨ Add Aliases again

---
+++ .config/oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,12 @@
+alias qq="exit"
+
+alias py='python'
+alias pyvenv='python -m venv env;source env/bin/activate'
+alias pyv="source env/bin/activate"
+
+alias ms='sudo mysql -u root -h localhost -p'
+
+alias rep='cd ~/Disk/Repos'
+alias dl="cd ~/Downloads"
+
+alias reload="exec ${SHELL} -l"
