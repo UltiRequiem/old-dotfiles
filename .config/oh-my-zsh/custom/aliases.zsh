f6cd8008711050dac99fad86a2340f6f9d655635
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-04-30 10:17:51 -0500

    ✨ Update

--- .config/oh-my-zsh/custom/aliases.zsh
+++ .config/oh-my-zsh/custom/aliases.zsh
@@ -6,7 +6,12 @@ alias pyv="source env/bin/activate"
 
 alias ms='sudo mysql -u root -h localhost -p'
 
+alias v="nvim"
+alias va="nvim ."
+
 alias rep='cd ~/Disk/Repos'
 alias dl="cd ~/Downloads"
+
+alias f="ranger"
 
 alias reload="exec ${SHELL} -l"
