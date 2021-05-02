049feeea415c0049fd7f8979edc76503e947e8a7
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-05-02 15:04:40 -0500

    ✨ Update

--- scripts/activate_second_monitor.sh
+++ scripts/activate_second_monitor.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-extra_monitor=$(xrandr --query | grep 'HDMI1')
+monitor_type="HDMI1"
+
+extra_monitor=$(xrandr --query | grep $monitor_type)
 
 if [[ $extra_monitor = *connected* ]]; then
+    echo "$monitor_type"
     xrandr --output HDMI1 --auto --right-of eDP1
 fi
