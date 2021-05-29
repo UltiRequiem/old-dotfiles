8303478362daeac4975002da056e9e39510af770
Eliaz Bobadilla <71897736+UltiRequiem@users.noreply.github.com>
2021-05-29 13:17:04 -0500

    Update aliases.zsh

--- .config/oh-my-zsh/custom/aliases.zsh
+++ .config/oh-my-zsh/custom/aliases.zsh
@@ -4,12 +4,10 @@ alias py='python'
 alias pyvenv='python -m venv env;source env/bin/activate'
 alias pyv="source env/bin/activate"
 
-alias ms='sudo mysql -u root -h localhost -p'
-
 alias v="nvim"
 alias va="nvim ."
 
-alias rep='cd ~/Disk/Repos'
+alias rp='cd ~/Disk/Repos'
 alias dl="cd ~/Downloads"
 
 alias f="ranger"
