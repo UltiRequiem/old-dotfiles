7b04b200eb8c8b73d5b8b7f261f9a90effde5036
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-04 13:35:51 -0500

    🚀 Update 2021-06-04

--- update.py
+++ update.py
@@ -1,13 +1,15 @@
 #!/bin/python3
 
-from os import system
+from os import listxattr, system
 from datetime import date
 
-list_of_files = []
+list_of_files = ["~/bin/*","~/.zshrc","~/.xinitrc","~/.vimrc","~/.gitconfig"]
+list_of_destination = ["bin","./.zshrc","./.xinitrc","./.vimrc","./.gitconfig"]
 
 
-def update_files(list_of_files):
-    pass
+def update_files(list_of_files,list_of_destination):
+    for i in range(len(list_of_files)):
+        system(f"cp {list_of_files[i]} {list_of_destination[i]} -r")
 
 
 def update_git():
@@ -16,5 +18,5 @@ def update_git():
 
 
 if __name__ == "__main__":
-    update_files(list_of_files)
+    update_files(list_of_files,list_of_destination)
     update_git()
