7356263a94f49cb686c5b210014b10d93183b781
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-06-04 13:56:37 -0500

    🚀 Update 2021-06-04

--- setup.py
+++ setup.py
@@ -1,16 +1,32 @@
 #!/bin/python3
 
-from os import listxattr, system
+from os import system
+
+dependencyes = []
+
+files_to_copy = ["bin/", "./.zshrc", "./.xinitrc", "./.vimrc", "./.gitconfig"]
+destinations = ["~/bin/*", "~/.zshrc", "~/.xinitrc", "~/.vimrc", "~/.gitconfig"]
 
 
-list_of_files = ["bin/", "./.zshrc", "./.xinitrc", "./.vimrc", "./.gitconfig"]
-list_of_destination = ["~/bin/*", "~/.zshrc", "~/.xinitrc", "~/.vimrc", "~/.gitconfig"]
+def install_yay():
+    try:
+        system("yay")
+    except FileNotFoundError:
+        system("pacman -S --needed git base-devel")
+        system("git clone https://aur.archlinux.org/yay.git")
+        system("cd yay; makepkg -si")
 
 
-def setup_system(list_of_files, list_of_destination):
+def install_dependecies():
+    pass
+
+
+def copy_files(list_of_files, list_of_destination):
     for i in range(len(list_of_files)):
         system(f"cp {list_of_files[i]} {list_of_destination[i]} -r")
 
 
 if __name__ == "__main__":
-    setup_system(list_of_files, list_of_destination)
+    install_yay()
+    install_dependecies()
+    copy_files(files_to_copy, destinations)
